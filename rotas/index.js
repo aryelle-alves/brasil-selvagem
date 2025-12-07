09f48f13e8168b8ecda1b19f6a29b0578543a61d
Aryelle Alves <aryellesofia25@gmail.com>
2025-12-06 23:39:10 -0300

    comentarios

--- rotas/index.js
+++ rotas/index.js
@@ -1,18 +1,12 @@
-// rotas/index.js
 const express = require('express');
 const router = express.Router();
 
-// Importar todas as rotas
 const autenticacaoRotas = require('./autenticacao.rotas');
 const usuarioRotas = require('./usuario.rotas');
 const paginasRotas = require('./paginas.rotas');
 const quizRotas = require('./quiz.rotas');
 
-// Agrupar rotas - ORDEM É CRÍTICA!
-// 1. Primeiro rotas específicas com parâmetros (:quizId)
-// 2. Depois rotas gerais
-
-router.use('/', quizRotas);         // Quiz - COLOCAR PRIMEIRO
+router.use('/', quizRotas);         // Quiz 
 router.use('/', paginasRotas);      // Páginas públicas
 router.use('/', autenticacaoRotas); // Autenticação
 router.use('/', usuarioRotas);      // Usuário
